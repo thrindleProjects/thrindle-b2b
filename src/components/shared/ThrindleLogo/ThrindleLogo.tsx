4d7cdd7dbaea875176c961793e611a8766d61bbb
khallekan <okeolalekanisaac@gmail.com>
2023-04-12 15:03:39 +0100

    feat: logo component

--- src/components/shared/ThrindleLogo/ThrindleLogo.tsx
+++ src/components/shared/ThrindleLogo/ThrindleLogo.tsx
@@ -1,28 +1,10 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
-const ThrindleLogo = () => {
+const ThrindleLogo: React.FC<{ variant: 'blue' | 'white' }> = ({ variant }) => {
   return (
-    <div className='aspect-video w-full bg-[url(/images/landing-hero-bg.png)] bg-cover bg-no-repeat'>
-      <div className='bg-primary-black/80 h-full w-full text-white'>
-        <header className='flex h-11 items-center justify-around'>
-          <figure className='relative h-full w-36'>
-            <Image alt='Logo' src='/svg/Logo.svg' fill={true} />
-          </figure>
-          <nav className='flex h-full items-center'>
-            <ul className='flex gap-6'>
-              <li className='font-extrabold'>How it works</li>
-              <li>Why Us</li>
-              <li>Contact us</li>
-            </ul>
-          </nav>
-
-          <Link href='/login' className='bg-primary-blue block'>
-            Login
-          </Link>
-        </header>
-      </div>
-    </div>
+    <figure className='relative h-full w-36'>
+      <Image alt='Logo' src={`/svg/Logo-${variant}.svg`} fill={true} />
+    </figure>
   );
 };
 
--- src/containers/LandingHero/LandingHero.tsx
+++ src/containers/LandingHero/LandingHero.tsx
@@ -1,5 +1,28 @@
-const LandingHero = () => {
-  return <div>LandingHero</div>;
+import Link from 'next/link';
+
+import ThrindleLogo from '@/components/shared/ThrindleLogo/ThrindleLogo';
+
+const LandingHero: React.FC = () => {
+  return (
+    <div className='aspect-video w-full bg-[url(/images/landing-hero-bg.png)] bg-cover bg-no-repeat'>
+      <div className='bg-primary-black/80 h-full w-full text-white'>
+        <header className='flex h-11 items-center justify-around'>
+          <ThrindleLogo variant='white' />
+          <nav className='flex h-full items-center'>
+            <ul className='flex gap-6'>
+              <li className='font-extrabold'>How it works</li>
+              <li>Why Us</li>
+              <li>Contact us</li>
+            </ul>
+          </nav>
+
+          <Link href='/login' className='bg-primary-blue block'>
+            Login
+          </Link>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default LandingHero;
